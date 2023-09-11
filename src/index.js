45e2fa7afcab20358defba206146a5a9ec2f9160
Piotr Roszkowski <pro@novicell.co.uk>
2023-09-11 15:04:51 +0200

    chat wip

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
+const path = require('path')
+const http = require('http')
 const express = require('express')
-const app = express()
-app.use(express.json())
+const socketio = require('socket.io')
 
-module.exports = app
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+
+io.on('connection' ,(socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
